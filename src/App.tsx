0746c28948ed97acbbd901e74804231c05c9d2fa
Jignesh Panchal <jignesh@capitalnumbers.com>
2023-07-03 19:51:55 +0530

    Redux statemange ment and tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+import { useDispatch } from 'react-redux';
+import { useTypedSelector } from './hooks /useTypeSelector';
+import { getComments } from './store/actionCreators/getComment';
+
+
+
+
 function App() {
+  const dispatch = useDispatch();
+  const [postId, setPostID] = useState("");
+  const { comments, loading, error } = useTypedSelector((state) => state.comments);
+
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await dispatch(getComments(postId) as any);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +30,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1 className="text-3xl font-bold underline text-green-600">
+          Simple React Typescript Tailwind Sample
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
