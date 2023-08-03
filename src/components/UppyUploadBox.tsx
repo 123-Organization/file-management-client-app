1ea9442bba36c7bb9ffcb1896e58d5a50f6a2803
jignesh panchal <jignesh@capitalnumbers.com>
2023-08-03 16:33:35 +0530

    Dropbox Integration

--- src/components/UppyUploadBox.tsx
+++ src/components/UppyUploadBox.tsx
@@ -4,15 +4,23 @@ import Uppy from '@uppy/core';
 import AwsS3 from '@uppy/aws-s3';
 import { Dashboard } from '@uppy/react';
 import GoogleDrive from '@uppy/google-drive';
+import Dropbox from '@uppy/dropbox';
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import axios from 'axios';
 
 const SERVER_BASE_URL = 'http://localhost:3020';
+const getTimeStamp = () => {
+  return Date.now()
+}
+const debugLogger = {
+  debug: (...args:any) => console.debug(`[Uppy] [${getTimeStamp()}]`, ...args),
+  warn: (...args:any) => console.warn(`[Uppy] [${getTimeStamp()}]`, ...args),
+  error: (...args:any) => console.error(`[Uppy] [${getTimeStamp()}]`, ...args),
+};
 
-
-const uppy = new Uppy({ autoProceed: false,
+const uppy = new Uppy({ logger: debugLogger , autoProceed: false,
   restrictions:{
     maxFileSize: (1024*1024*80),
     maxNumberOfFiles: 20,
@@ -25,6 +33,9 @@ const uppy = new Uppy({ autoProceed: false,
   .use(GoogleDrive, {
     companionUrl: `${SERVER_BASE_URL}`,
   })
+  .use(Dropbox, {
+    companionUrl: `${SERVER_BASE_URL}`,
+  })
   .on("complete", (result) => {
     if (result.failed.length === 0) {
       console.log("Upload successful");
@@ -34,16 +45,13 @@ const uppy = new Uppy({ autoProceed: false,
     console.log("successful files:", result.successful);
     console.log("failed files:", result.failed);
   });
-  // .use(GoogleDrive, {
-  //   companionUrl: 'http://localhost:5000/api/',
-  // });
 
 
 const UppyUploadBox: React.FC = () : JSX.Element => {
   return (
     <div className="flex justify-end items-center pt-6">
 
-      <Dashboard  uppy={uppy} plugins={['GoogleDrive']} />
+      <Dashboard  uppy={uppy} plugins={['GoogleDrive','Dropbox']} />
     </div>
   )
 }
