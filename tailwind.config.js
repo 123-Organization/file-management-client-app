6ca583b323274255aa871958d7882c02668b9fbd
momen-cn <momen@capitalnumbers.com>
2024-10-22 09:48:46 +0300

    fix[frontend]: fixing the front end dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
+  darkMode: 'class', 
   plugins: [],
   screens: {
     'smg': '300px',
   },
-}
-
+};
